8124034aedbc1079769a9fc98a7deb90d52cb797
Toni__SM <a.santaella.m@gmail.com>
2023-09-08 18:37:15 +0200

    Add swc-loader dev deps

--- build/290.2980295d.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmedusa_starter_default=self.webpackChunkmedusa_starter_default||[]).push([[290],{7290:(e,t,s)=>{s.r(t),s.d(t,{default:()=>a});let a=[{identifier:"local",extensions:[]}]}}]);
