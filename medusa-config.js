cbdb462319599e8b1b86ea2f1972d18bcd1c336c
Toni__SM <a.santaella.m@gmail.com>
2024-10-27 23:39:56 +0100

    Payments: set automatic capture options to stripe & paypal payment

--- medusa-config.js
+++ medusa-config.js
@@ -82,6 +82,7 @@ const plugins = [
     options: {
       clientId: process.env.PAYPAL_CLIENT_ID,
       clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+      capture: true
     },
   },
   {
@@ -89,7 +90,7 @@ const plugins = [
     options: {
       api_key: process.env.STRIPE_API_KEY,
       webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-      capture: true,
+      capture: "automatic",
     },
   },
 
