c0afab4b0f5aea45889891fa72b1f4bc6a820755
Toni__SM <a.santaella.m@gmail.com>
2025-02-12 11:24:16 +0100

    Order invoice service: add cors to endpoint

--- src/api/store/orders/[id]/invoice/route.ts
+++ src/api/store/orders/[id]/invoice/route.ts
@@ -1,24 +1,39 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/medusa";
+import cors from "cors";
+
+const origin = process.env.STORE_CORS
+  ? process.env.STORE_CORS.split(",")
+  : /\.cartago4x4\.es$/;
+
+console.log("Origin CORS => ", origin);
+const corsOptions = {
+  origin,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: true,
+};
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-  const { id } = req.params;
+  cors(corsOptions)(req, res, async () => {
+    const { id } = req.params;
 
-  try {
-    const invoiceService = req.scope.resolve("invoiceGeneratorService");
-   
-    const pdf = await invoiceService.generateInvoice(id);
-    console.log("PDF filename =>>", pdf.fileName);
+    try {
+      const invoiceService = req.scope.resolve("invoiceGeneratorService");
 
-    res.set({
-      "Content-Type": "application/pdf",
-      "Content-Disposition": `attachment; filename="Cartago4x4"`,      
-      "Content-Length": pdf.buffer.length,
-    });
+      const pdf = await invoiceService.generateInvoice(id);
+      console.log("PDF filename =>>", pdf.fileName);
 
-    // Enviar el PDF como respuesta
-    res.end(pdf.buffer);
-  } catch (error) {
-    console.error("Error generating invoice:", error);
-    res.status(500).send("Failed to generate invoice");
-  }
+      res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename="Cartago4x4"`,
+        "Content-Length": pdf.buffer.length,
+      });
+
+      // Enviar el PDF como respuesta
+      res.end(pdf.buffer);
+    } catch (error) {
+      console.error("Error generating invoice:", error);
+      res.status(500).send("Failed to generate invoice");
+    }
+  });
 };
