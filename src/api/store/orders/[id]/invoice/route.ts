5b808f517fe5312630bc342b1b75f796e10fd901
Toni__SM <a.santaella.m@gmail.com>
2025-02-13 02:00:47 +0100

    Order invoice service: add OPTION method to apit route

--- src/api/store/orders/[id]/invoice/route.ts
+++ src/api/store/orders/[id]/invoice/route.ts
@@ -14,7 +14,8 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
       "Content-Disposition": `attachment; filename="Cartago4x4"`,
       "Content-Length": pdf.buffer.length,
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": " Content-Type, Authorization, X-Requested-With",
     });
 
     // Enviar el PDF como respuesta
@@ -23,5 +24,16 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     console.error("Error generating invoice:", error);
     res.status(500).send("Failed to generate invoice");
   }
+};
+
+export const OPTIONS = async (req: MedusaRequest, res: MedusaResponse) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+  });
+  
+  // Enviar respuesta sin cuerpo (status 204 o 200)
+  return res.status(204).send();
 };
 //export const CORS = false;
