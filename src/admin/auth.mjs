c3fc098246f4f383a1cabc921e02cd714e3a36b6
Toni__SM <a.santaella.m@gmail.com>
2025-02-18 01:02:13 +0100

    Clean garbage: admin folder

--- src/admin/auth.mjs
+++
@@ -1,9 +0,0 @@
-import Medusa from "@medusajs/medusa-js";
-const medusa = new Medusa({
-  baseUrl: "http://localhost:9000",
-  maxRetries: 3,
-});
-await medusa.admin.auth.getToken({
-  email: "admin@medusa-test.com",
-  password: "supersecret",
-});
