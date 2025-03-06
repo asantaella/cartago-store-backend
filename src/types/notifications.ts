6bc73a1e885970be0b30e356e2f963744d7ffe4c
Toni__SM <a.santaella.m@gmail.com>
2025-03-06 15:57:14 +0100

    Typing: notifications

---
+++ src/types/notifications.ts
@@ -0,0 +1,17 @@
+import { Order, ShippingMethod } from "@medusajs/medusa";
+
+export type NotificationSender = {
+  Sender_Name: string;
+  Sender_Address: string;
+  Sender_City: string;
+  Sender_State: string;
+};
+
+export type NotificationOrder = Partial<Order> & {
+  date: Date;
+  dateFormat: string;
+  subtotal_ex_tax: string;
+  shipping_method: ShippingMethod;
+};
+
+export type NotificationContent = NotificationSender & NotificationOrder;
